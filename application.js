5b0769b9af98c0539cb05218ee364876356f40a6
Ann Catton <anncatton@gmail.com>
2015-11-27 20:50:20 +0000

    can click on city name to retrieve current conditions

--- application.js
+++ application.js
@@ -2,10 +2,11 @@ $(function() {
 
   var API_KEY = "905ae13579633139";
   var searchTemplate = _.template($('#search-template').html());
+  var conditionsTemplate = _.template($('#conditions-template').html());
 
   $('#city-search').submit(function(event) {
     event.preventDefault();
-
+    
     var query = $('#city-name').val();
     var request = {
       url: 'http://autocomplete.wunderground.com/aq',
@@ -25,16 +26,47 @@ $(function() {
           longitude: city.lon,
         });
       });
+
       $('#result').html(output);
+    });
+
+  });
+  $('#result').on('click', '.city-data', function(event) {
+    event.preventDefault();
+    var cityName = $(this).children('.city').text().split(",");
+    var euroCountry = cityName[1].replace(" ", "");
+    var countryName = $(this).children('.country').text();
+    if (countryName == "US") {
+      console.log("It's American!");
+    } else {
+      var request = {
+        url: 'http://api.wunderground.com/api/' + API_KEY + '/conditions/q/' + euroCountry + "/" + cityName[0] + '.json',
+      };
+    }
+
+    $.ajax(request).then(function(data) {
+      var output = '';
+      var conditions = data.current_observation
+      
+      output += conditionsTemplate({
+        city: cityName.join(),
+        feelsLike: conditions.feelslike_c,
+        weather: conditions.weather,
+        tempC: conditions.temp_c,
+        obsTime: conditions.observation_time,
+      });
+      $('#result').hide();
+      $('#conditions').html(output);
     });
 
   });
 
 });
 
+// http://api.wunderground.com/api/905ae13579633139/conditions/q/CA/San_Francisco.json
+// http://api.wunderground.com/api/API_KEY/conditions/q/CountryorState/CityName.json
 
-
-
+//GET http://api.wunderground.com/api/905ae13579633139/features/settings/q/query.format
 
 
 
